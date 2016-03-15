fd736adcbba87359357c33e1e3f93274280f5217
Daniel Klose <daniel@Daniels-MacBook-Air.local>
2016-03-15 09:35:59 +0100

    updates

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ function Initialize(Plugin)
 
 	-- Hooks
   cPluginManager:AddHook(cPluginManager.HOOK_CHUNK_AVAILABLE, MyOnChunkAvailable);
-  cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_BROKEN_BLOCK, MyOnPlayerBrokenBlock);
   cPluginManager:AddHook(cPluginManager.HOOK_UPDATING_SIGN, MyOnUpdatingSign);
 	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_BREAKING_BLOCK, MyOnPlayerBreakingBlock);
 
@@ -27,7 +26,7 @@ function OnDisable()
 end
 
 function MyOnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
-	if fences[BlockX] == nil and fences[BlockX][BlockZ] == nil then	-- belongs the block to nobody?
+	if fences[BlockX] == nil or fences[BlockX][BlockZ] == nil then	-- belongs the block to nobody?
 		return false
 	end
 	if fences[BlockX][BlockZ][Player:GetName()] == true then -- belongs the block to the player breaking it?
@@ -62,52 +61,60 @@ function MyOnChunkAvailable(World, ChunkX, ChunkZ)
   end
 end
 
-function process_garden(World, X, Y, Z, register, Player_name)
+function process_garden(World, X, Y, Z, register, Player_name, friend1, friend2)
   if check_fence_gate(World, X, Y, Z) == false then
     return false
   end
 	LOG("gate is working")
-  if check_fence(World, X, Y, Z, register, Player_name) == false then
+  if check_fence(World, X, Y, Z, register, Player_name, friend1, friend2) == false then
 		LOG("error")
     return false
   end
 	if register == false then
 		LOG("registering now")
-		process_garden(World, X, Y, Z, true, Player_name)
+		process_garden(World, X, Y, Z, true, Player_name, friend1, friend2)
 	end
 	return true
 end
 
-function fence_function(World, X, Y, Z, D, register, Player_name)
-	LOG(Player_name)
+function register(X, Z, Player_name)
+	fences[X][Z][Player_name] = true
+	fences[X + 1][Z][Player_name] = true
+	fences[X + 1][Z + 1][Player_name] = true
+	fences[X + 1][Z - 1][Player_name] = true
+	fences[X - 1][Z][Player_name] = true
+	fences[X - 1][Z + 1][Player_name] = true
+	fences[X + 1][Z - 1][Player_name] = true
+	fences[X][Z + 1][Player_name] = true
+	fences[X][Z - 1][Player_name] = true
+	return true
+end
+
+function fence_function(World, X, Y, Z, D, register, Player_name, friend1, friend2)
 	if register then
-		LOG("1")
 		if fences[X] == nil then
-			LOG("2")
 			fences[X] = {}
 		end
 		if fences[X][Z] == nil then
-			LOG("3")
 			fences[X][Z] = {}
 		end
 		if fences[X][Z][Player_name] == nil then
-			LOG("4")
-			LOG(Player_name)
-			fences[X][Z][Player_name] = true
-			LOG(X .. " " .. Z .. " " .. Player_name)
+			register(X, Z, Player_name)
+			if friend1 ~= nil then
+				register(X, Z, friend1)
+			end
+			if friend2 ~= nil then
+				register(X, Z, friend2)
+			end
 		end
 	end
 	if (World:GetBlock(X + 1, Y, Z) == 85 or World:GetBlock(X + 1, Y, Z) == 107) and D ~= "x-" then
-		LOG("left")
 		return X + 1, Y, Z, "x+"
 	else if (World:GetBlock(X, Y, Z + 1) == 85 or World:GetBlock(X, Y, Z + 1) == 107) and D ~= "z-" then
-		LOG("up")
 			return X, Y, Z + 1, "z+"
 		else if (World:GetBlock(X - 1, Y, Z) == 85 or World:GetBlock(X - 1, Y, Z) == 107) and D ~= "x+" then
-			LOG("right")
 				return X - 1, Y, Z, "x-"
 			else if (World:GetBlock(X, Y, Z - 1) == 85 or World:GetBlock(X, Y, Z - 1) == 107) and D ~= "z+" then
-				LOG("down")
 					return X, Y, Z - 1, "z-"
 				end
 			end
@@ -116,18 +123,17 @@ function fence_function(World, X, Y, Z, D, register, Player_name)
 	return false
 end
 
-function check_fence(World, X, Y, Z, register, Player_name)
+function check_fence(World, X, Y, Z, register, Player_name, friend1, friend2)
   X, Y, Z = check_fence_gate(World, X, Y, Z)
   local X_fence, Y_fence, Z_fence = X, Y, Z
-	X_fence, Y_fence, Z_fence, D_fence = fence_function(World, X_fence, Y_fence, Z_fence, "", register, Player_name)
+	X_fence, Y_fence, Z_fence, D_fence = fence_function(World, X_fence, Y_fence, Z_fence, "", register, Player_name, friend1, friend2)
 	LOG(X_fence .. " " .. Y_fence .. " " .. Z_fence .. " " .. D_fence)
 	LOG(X .. " " .. Y .. " " .. Z)
   while X ~= X_fence or Y ~= Y_fence or Z ~= Z_fence do
-		X_fence, Y_fence, Z_fence, D_fence = fence_function(World, X_fence, Y_fence, Z_fence, D_fence, register, Player_name)
+		X_fence, Y_fence, Z_fence, D_fence = fence_function(World, X_fence, Y_fence, Z_fence, D_fence, register, Player_name, friend1, friend2)
 		if X_fence == false then
 			return false
 		end
-		LOG("fence_registered")
   end
 	LOG("finished")
 	return true
@@ -164,14 +170,10 @@ end
 
 function MyOnUpdatingSign(World, BlockX, BlockY, BlockZ, Line1, Line2, Line3, Line4, Player)
     if Line1 == "[garden]" then
-      if process_garden(World, BlockX, BlockY, BlockZ, false, Player:GetName()) then
+      if process_garden(World, BlockX, BlockY, BlockZ, false, Player:GetName(), Line3, Line4) then
         Player:SendMessage("you have created a garden")
         return false, Line1, Player:GetName(), Line3, Line4;
       end
       Player:SendMessage("your garden is broken")
     end
-end
-
-function MyOnPlayerBrokenBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
-  LOG(BlockType)
 end
